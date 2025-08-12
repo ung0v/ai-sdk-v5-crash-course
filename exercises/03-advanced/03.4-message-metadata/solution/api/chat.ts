b3016c7cccab8d28826ba108fe08eff7da6adbfe
Matt Pocock <mattpocockvoice@gmail.com>
2025-08-12 16:42:05 +0100

    Added solution

---
+++ exercises/03-advanced/03.4-message-metadata/solution/api/chat.ts
@@ -0,0 +1,35 @@
+import { google } from '@ai-sdk/google';
+import {
+  convertToModelMessages,
+  streamText,
+  type UIMessage,
+} from 'ai';
+
+export type MyUIMessage = UIMessage<{
+  duration: number;
+}>;
+
+export const POST = async (req: Request): Promise<Response> => {
+  const body: { messages: MyUIMessage[] } = await req.json();
+  const { messages } = body;
+
+  const result = streamText({
+    model: google('gemini-2.5-flash'),
+    messages: convertToModelMessages(messages),
+  });
+
+  const startTime = Date.now();
+
+  return result.toUIMessageStreamResponse<MyUIMessage>({
+    originalMessages: messages,
+    messageMetadata({ part }) {
+      if (part.type === 'finish') {
+        return {
+          duration: Date.now() - startTime,
+        };
+      }
+
+      return undefined;
+    },
+  });
+};
---
+++ exercises/03-advanced/03.4-message-metadata/solution/client/root.tsx
@@ -0,0 +1,41 @@
+import { useChat } from '@ai-sdk/react';
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChatInput, Message, Wrapper } from './components.tsx';
+import './tailwind.css';
+import type { MyUIMessage } from '../api/chat.ts';
+
+const App = () => {
+  const { messages, sendMessage } = useChat<MyUIMessage>({});
+
+  const [input, setInput] = useState(
+    `Give me the opening paragraph of a book about a detective cat.`,
+  );
+
+  return (
+    <Wrapper>
+      {messages.map((message) => (
+        <Message
+          key={message.id}
+          role={message.role}
+          parts={message.parts}
+          metadata={message.metadata}
+        />
+      ))}
+      <ChatInput
+        input={input}
+        onChange={(e) => setInput(e.target.value)}
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage({
+            text: input,
+          });
+          setInput('');
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />);
