6135c72d54d94aa527fd6c6f4f7fb580a553e74e
Matt Pocock <mattpocockvoice@gmail.com>
2025-09-08 10:51:44 +0100

    Added usage exercise

---
+++ exercises/02-llm-fundamentals/02.03-usage/problem/main.ts
@@ -0,0 +1,16 @@
+import { google } from '@ai-sdk/google';
+import { streamText } from 'ai';
+
+const output = streamText({
+  model: google('gemini-2.0-flash-lite'),
+  prompt: `Which country makes the best sausages? Answer in a single paragraph.`,
+});
+
+for await (const chunk of output.textStream) {
+  process.stdout.write(chunk);
+}
+
+console.log(); // Empty log to separate the output from the usage
+
+// TODO: Print the usage to the console
+TODO;
