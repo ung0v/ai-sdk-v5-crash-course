9150c4adbfe952ceaa2d2db759679374143f42e7
Matt Pocock <mattpocockvoice@gmail.com>
2025-09-08 11:46:49 +0100

    Updates

--- exercises/02-llm-fundamentals/02.02-data-represented-as-tokens/explainer/main.ts
+++ exercises/02-llm-fundamentals/02.02-data-represented-as-tokens/explainer/main.ts
@@ -1,7 +1,5 @@
 import { Tiktoken } from 'js-tiktoken/lite';
 import o200k_base from 'js-tiktoken/ranks/o200k_base';
-import { readFileSync } from 'node:fs';
-import path from 'node:path';
 
 const tokenizer = new Tiktoken(
   // NOTE: o200k_base is the tokenizer for GPT-4o
